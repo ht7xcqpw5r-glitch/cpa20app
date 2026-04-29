979eded0ee6f3389edaebd5a7eccf19c664c39da
ht7xcqpw5r-glitch <ht7xcqpw5r@privaterelay.appleid.com>
2026-04-29 03:11:24 -0400

    Update service-worker.js
    Signed-off-by: ht7xcqpw5r-glitch <ht7xcqpw5r@privaterelay.appleid.com>

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-const CACHE_NAME="cpa-app";
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open(CACHE_NAME).then(c=>c.addAll(["./"]))
- );
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
-});
+self.addEventListener("fetch", () => {});
